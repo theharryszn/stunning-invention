a4e2764f0a5715014482dfa92b1c99bf0cb78331
Ope <opeabidemi@gmail.com>
2022-06-14 06:11:54 +0100

    Initial Commit

---
+++ Shared/swiftlyApp.swift
@@ -0,0 +1,17 @@
+//
+//  swiftlyApp.swift
+//  Shared
+//
+//  Created by Harry on 14/06/2022.
+//
+
+import SwiftUI
+
+@main
+struct swiftlyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
