bcc4abef10f1173a4ae7c1b939239736add889b3
Ope <opeabidemi@gmail.com>
2022-07-03 20:07:12 +0100

    designed fun fact tab

--- Shared/FunFactsView.swift
+++ Shared/FunFactsView.swift
@@ -8,8 +8,25 @@
 import SwiftUI
 
 struct FunFactsView: View {
+    
+    @State private var funFact = ""
     var body: some View {
-        Text("Fun Facts")
+        VStack {
+            Text("Fun Facts")
+                .font(.largeTitle)
+                .fontWeight(.bold)
+            
+            Text(funFact)
+               .padding()
+               .font(.title)
+               .frame(minHeight: 400)
+
+            Button("Show Random Fact") {
+               funFact = information.funFacts.randomElement()!
+            }
+            
+        }
+        .padding()
     }
 }
 
